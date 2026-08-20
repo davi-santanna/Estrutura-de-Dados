0ba10155ed3727e8d09a9e79df074b51d7f1def5
davi-santanna <davisantanna0706@gmail.com>
2026-08-19 22:16:45 -0300

    add percorrer pos ordem sem recursao

---
+++ arvore-binaria-pos-ordem/arvore-binaria.c
@@ -0,0 +1,104 @@
+#include "arvore-binaria.h"
+
+TNoA *inicializa(void) {
+    return NULL;
+}
+
+void imprime(TNoA *nodo, int tab) {
+    for (int i = 0; i < tab; i++) {
+        printf("-");
+    }
+    if (nodo != NULL) {
+        printf("%c\n", nodo->info);
+        imprime(nodo->esq, tab + 2);
+        printf("\n");
+        imprime(nodo->dir, tab + 2);
+    } else printf("vazio");
+}
+
+TNoA *criaNo(char ch) {
+    TNoA *novo;
+    novo = (TNoA *) malloc(sizeof(TNoA));
+    novo->info = ch;
+    novo->esq = NULL;
+    novo->dir = NULL;
+    return novo;
+}
+
+int main(void) {
+    TNoA *raiz;
+    raiz = criaNo('A');
+    raiz->esq = criaNo('B');
+    raiz->dir = criaNo('C');
+    raiz->dir->esq = criaNo('D');
+    raiz->dir->dir = criaNo('E');
+    percorrePosOrdem(raiz);
+
+}
+
+//criando funcoes para pilha
+
+void inicializaPilha(pilha*p) {p->topo = NULL;}
+
+void push(pilha*p, TNoA* infoNoArvore){
+    noPilha* novoTopo = (noPilha*)malloc(sizeof(noPilha));
+    novoTopo->conteudoNo = infoNoArvore;
+    novoTopo->prox = p->topo;
+    p->topo = novoTopo;
+    }
+
+TNoA* pop(pilha*p){
+    if(p&&p->topo){
+        noPilha* temp;
+        TNoA* conteudo;
+        temp = p->topo;
+        conteudo = temp->conteudoNo;
+        p->topo = p->topo->prox;
+        free(temp);
+        return conteudo;
+    }
+    else return NULL;
+}
+
+void percorreSimetrica(TNoA* raiz)
+{
+    pilha p;
+    inicializaPilha(&p);
+    TNoA*atual = raiz;
+
+    while(atual != NULL|| p.topo != NULL){
+        while(atual != NULL){
+            push(&p, atual);
+            atual = atual->esq;
+        }
+        atual = pop(&p);
+        printf("%c", atual->info);
+        atual = atual->dir;
+    }
+}
+
+void percorrePosOrdem(TNoA* raiz)
+{
+    pilha p;
+    inicializaPilha(&p);
+    TNoA *atual = raiz;
+    TNoA *ultimoVisitado = NULL;
+
+    while (atual != NULL || p.topo != NULL) {
+        if (atual != NULL) {
+            push(&p, atual);
+            atual = atual->esq;
+        } else {
+            TNoA *topoNo = p.topo->conteudoNo;      
+            if (topoNo->dir != NULL && topoNo->dir != ultimoVisitado) {
+                atual = topoNo->dir;                
+            } 
+            else {
+                printf("%c", topoNo->info);
+                ultimoVisitado = pop(&p);           
+            }
+        }
+    }
+}
+
+
---
+++ arvore-binaria-pos-ordem/arvore-binaria.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct noA {
+    char info;
+    struct noA *esq;
+    struct noA *dir;
+} TNoA;
+
+typedef struct noPilha{
+    TNoA* conteudoNo;
+    struct noPilha *prox;
+} noPilha;
+
+typedef struct pilha{
+    noPilha *topo;
+}pilha;
+
+
+TNoA *inicializa(void);
+void imprime(TNoA *nodo, int tab);
+TNoA *criaNo(char ch);
+void inicializaPilha(pilha *p);
+void push(pilha *p, TNoA *infoNoArvore);
+TNoA *pop(pilha *p);
+void percorreSimetrica(TNoA *raiz);
+void percorrePosOrdem(TNoA *raiz);
